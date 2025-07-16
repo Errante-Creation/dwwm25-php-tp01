a1e537abba10fc96557386b934e0820872039afb
JackAdamsJenkins <clensx@gmail.com>
2025-07-16 09:56:24 +0200

    chore(database): remove old database files
    - Move database.png
    - Move database.sql
    - Added new SQL files for cities,
      countries, and continents

---
+++ database/continents.sql
@@ -0,0 +1,5 @@
+insert into continents (name_continent) values ('diam vitae');
+insert into continents (name_continent) values ('at');
+insert into continents (name_continent) values ('aliquet maecenas');
+insert into continents (name_continent) values ('nulla ultrices');
+insert into continents (name_continent) values ('in');
