ec6c54650f33b5cfc6d009833c71d25cc1ef9466
JackAdamsJenkins <clensx@gmail.com>
2025-07-16 12:21:21 +0200

    feat(cities): add city management
    - Added form to add new cities.
    - Implemented input validation.
    - Added database functions for cities.
    - Included region selection dropdown.
    - Improved error and success messages.

--- addcity.php
+++ addcity.php
@@ -1,3 +1,60 @@
+<?php
+   $errorMessage = "";
+   $isError = false;
+   if(!empty($_POST['submit'])){
+      if(empty($_POST['region']) || $_POST['region'] == 0){
+         $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">Vous devez renseigner une région</span>';
+         $isError = true;
+      }
+
+      if(!$isError && empty($_POST['city'])){
+         $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">Vous devez renseigner la ville</span>';
+         $isError = true;
+      }
+
+      if(!$isError && empty($_POST['area']) || $_POST['area'] <= 0){
+         $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">Vous devez renseigner la superficie de la ville</span>';
+         $isError = true;
+      }
+
+      if(!$isError && empty($_POST['pop']) || $_POST['pop'] <= 0){
+         $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">Vous devez renseigner la population de la ville</span>';
+         $isError = true;
+      }
+
+      // Si on arrive ici, c'est qu'il n'y a PAS d'erreur
+      if(!$isError){
+         // On peut traiter le formulaire :
+            // Faire une requête SELECT pour vérifier que la ville n'est pas déjà présente sur cette région
+            // On insère dans la base de données
+         $region = $_POST['region'];
+         $city = $_POST['city'];
+         $area = $_POST['area'];
+         $pop = $_POST['pop'];
+         
+         require_once "config/connect.php";
+         require_once "functions.php";
+         $isAlreadyExists = checkIfCityExists($pdo, $region, $city);
+         // 1 → ce n'est pas bon c'est un doublon, 0 → on peut ajouter
+         if($isAlreadyExists){
+            $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">La ville existe déjà dans cette région</span>';
+            $isError = true;
+         } else {
+            // On aojoute la ville à la BDD
+            $request = addCity($pdo, $region, $city, $area, $pop);
+            if($request){
+               $errorMessage = '<span style="color:green; font-weight:bold; font-size:120%;">La ville a été ajoutée</span>';
+            } else {
+               $errorMessage = '<span style="color:red; font-weight:bold; font-size:120%;">La ville n\'a pas été ajoutée</span>';
+            }
+         }
+         
+         
+      }
+
+
+   }
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,9 +65,35 @@
 <body>
    <h1>Ajouter une ville</h1>
    <a href="index.php">← Retour à la liste</a>
-
+   <p>Ajouter une nouvelle ville à une région existante</p>
+   <?= $errorMessage ?>
    <form action="#" method="post">
-      
+      <label for="region">Région : </label>
+      <select name="region" id="region">
+         <option value="0">Veuillez sélectionner une région</option>
+         <?php 
+            require_once "config/connect.php";
+            require_once "functions.php";
+            foreach(getRegions($pdo) as $region):
+               echo "<option value='$region[id_region]'>$region[name_region]</option>";
+            endforeach;
+         ?>
+      </select> 
+      <br />
+      <br />
+      <label for="city">Ville : </label>
+      <input type="text" name="city" required>
+      <br />
+      <br />
+      <label for="area">Superficie : </label>
+      <input type="number" name="area" required>
+      <br />
+      <br />
+      <label for="pop">Population : </label>
+      <input type="number" name="pop" required>
+      <br />
+      <br />
+      <input type="submit" name="submit" value="Ajouter">
    </form>
    
 </body>
--- functions.php
+++ functions.php
@@ -31,3 +31,56 @@ function getCitiesByCountryId($pdo, $idCountry){
    ));
    return $stmt->fetchAll();
 }
+
+/**
+ * Retrieves all regions from the database.
+ *
+ * @param PDO $pdo The PDO database connection object.
+ * @return array An array of regions, each containing 'id_region' and 'name_region'.
+ */
+function getRegions($pdo){
+   $stmt=$pdo->prepare('SELECT id_region, name_region FROM regions');
+   $stmt->execute();
+   return $stmt->fetchAll();
+}
+
+/**
+ * Checks if a city exists in the database for a given region.
+ *
+ * @param PDO    $pdo    The PDO database connection object.
+ * @param string $region The region identifier or name to search for.
+ * @param string $city   The name of the city to check.
+ *
+ * @return bool Returns true if the city exists in the specified region, false otherwise.
+ */
+function checkIfCityExists($pdo, $region, $city){
+   $stmt=$pdo->prepare('SELECT COUNT(id_city) as nb FROM cities WHERE name_city=:city AND fk_id_region=:region');
+   $stmt->execute(array(
+      'city' => $city,
+      'region' => $region
+   ));
+   $datas = $stmt->fetch();
+   return ($datas['nb'] >= 1) ? true : false;
+}
+
+/**
+ * Adds a new city to the database.
+ *
+ * @param PDO    $pdo    The PDO database connection object.
+ * @param int    $region The ID of the region to associate with the city.
+ * @param string $city   The name of the city to add.
+ * @param float  $area   The area of the city.
+ * @param int    $pop    The population of the city.
+ *
+ * @return int The number of rows affected by the insert operation.
+ */
+function addCity($pdo, $region, $city, $area, $pop){
+   $stmt=$pdo->prepare('INSERT INTO cities(name_city, area_city, pop_city, fk_id_region) VALUES(:city, :area, :pop, :region)');
+   $stmt->execute(array(
+      'city' => $city,
+      'area' => $area,
+      'pop' => $pop,
+      'region' => $region
+   ));
+   return $stmt->rowCount();
+}
